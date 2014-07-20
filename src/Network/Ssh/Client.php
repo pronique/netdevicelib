b6ace91e7e6ead0bb5953e4dfadb29bdb0e9f881
joncutrer <joncutrer@gmail.com>
2014-07-20 09:55:22 -0500

    moved classes to src/Vendor

--- src/Network/Ssh/Client.php
+++ src/Network/Ssh/Client.php
@@ -29,13 +29,63 @@ class Client {
  */
 	protected $_defaultConfig = [
 		'host' => null,
-		'port' => null,
+		'port' => 22,
 		'scheme' => 'ssh2',
-		'timeout' => 30
+		'timeout' => 12,
+		'user'=>null,
+		'pass'=>null
 	];
+
+	protected $_connection;
+
+	protected $authenticated = false;
 
 	public function __construct($config = []) {
 		$this->config($config);
 	}
+
+	public function connect( $host='', $port='' ) {
+		//Check to see if already connected
+		if ( $this->_connection ) { return true; }
+
+		if ( $host ) { $this->_configWrite('host', $host); }
+		if ( $port ) { $this->_configWrite('port', $port); }
+
+		$this->_connection = @ssh2_connect( $this->_configRead('host'), $this->_configRead('port') );
+
+		if ( $this->_connection ) {
+			return true;
+		}
+		return false;
+	}
+
+	public function disconnect() {
+		unset( $this->_connection );
+		$this->authenticated = false;
+		return true;
+	}
+
+	public function auth_password( $user='', $pass='') {
+		// check to see if already authenticated
+		if ( $this->authenticated === true ) { return true; }
+
+		if ( $user ) { $this->_configWrite('user', $user); }
+		if ( $pass ) { $this->_configWrite('pass', $pass); }
+
+		if ( ssh2_auth_password($this->_connection, $user, $pass) ) {
+			$this->authenticated = true;
+			return true;
+		}
+		return false;
+	}
+
+	public function exec( $cmd ) {
+		$stream = ssh2_exec($this->_connection, $cmd);
+    stream_set_blocking($stream, true);
+    return stream_get_contents($stream); 
+
+	}
+
+
 
 }
